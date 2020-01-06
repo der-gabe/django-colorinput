0d1234adb8b1b7f29aac3f313f2c9381f7786f23
Gabe Niebler <gabe@zero.codepage.cf>
2020-01-06 23:52:15 +0100

    Test formfield rendering

--- colorinput/tests/settings.py
+++ colorinput/tests/settings.py
@@ -4,4 +4,14 @@ DATABASES = {
         'NAME': ':memory:',
         }
 }
+INSTALLED_APPS = [
+    'colorinput',
+]
 SECRET_KEY = 'not very secret at all, actually'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+    },
+]
--- colorinput/tests/test_formfields.py
+++ colorinput/tests/test_formfields.py
@@ -1,6 +1,17 @@
+from django.forms import Form
+
 from ..forms.fields import ColorField
+
 
 def test_formfield_clean():
     formfield = ColorField()
     cleaned_value = formfield.clean('#abcdef')
     assert cleaned_value == 'abcdef'
+
+def test_formfield_rendering():
+    class TestForm(Form):
+        color = ColorField(initial='d0d0d0')
+    test_form = TestForm()
+    html_output = test_form.as_p()
+    assert('<input type="color" name="color" '
+           'value="#d0d0d0" required id="id_color">' in html_output)
