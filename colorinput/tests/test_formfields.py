3c16e9ab3adf9eca4d49f5caf5b6288ed2785110
Gabe Niebler <gabe@zero.codepage.cf>
2020-01-11 23:07:06 +0100

    Use hypothesis to test form field rendering

--- colorinput/tests/test_formfields.py
+++ colorinput/tests/test_formfields.py
@@ -17,23 +17,26 @@ def test_formfield_clean_error(value):
         formfield.clean(value)
 
 @given(value=text(alphabet=HEX_ALPHABET, min_size=6, max_size=8))
-@example('0d0d0d')
+@example('d0d0d0')
 def test_formfield_clean_success(value):
     formfield = ColorField()
     cleaned_value = formfield.clean('#' + value)
     assert cleaned_value == value
 
 @given(value=text(alphabet=HEX_ALPHABET, min_size=6, max_size=6))
-@example('0d0d0d')
+@example('d0d0d0')
 def test_formfield_prepare_value(value):
     formfield = ColorField()
     prepared_value = formfield.prepare_value(value)
     assert prepared_value == '#' + value
 
-def test_formfield_rendering():
+@given(value=text(alphabet=HEX_ALPHABET, min_size=6, max_size=6))
+@example('d0d0d0')
+def test_formfield_rendering(value):
     class TestForm(Form):
-        color = ColorField(initial='d0d0d0')
+        color = ColorField(initial=value)
     test_form = TestForm()
     html_output = test_form.as_p()
     assert('<input type="color" name="color" '
-           'value="#d0d0d0" required id="id_color">' in html_output)
+           'value="#{}" required id="id_color">'.format(value)
+           in html_output)
