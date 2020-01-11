b9109191a2aec4d1de001df1031a1473702231e4
Gabe Niebler <gabe@zero.codepage.cf>
2020-01-11 22:43:59 +0100

    Test form field's `prepare_value` method
    … and use 'hypothesis' to do it!

--- colorinput/tests/test_formfields.py
+++ colorinput/tests/test_formfields.py
@@ -1,12 +1,24 @@
 from django.forms import Form
+from hypothesis import example, given
+from hypothesis.strategies import text
 
 from ..forms.fields import ColorField
+
+
+HEX_ALPHABET = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'}
 
 
 def test_formfield_clean():
     formfield = ColorField()
     cleaned_value = formfield.clean('#abcdef')
     assert cleaned_value == 'abcdef'
+
+@given(value=text(alphabet=HEX_ALPHABET, min_size=6, max_size=6))
+@example('0d0d0d0')
+def test_formfield_prepare_value(value):
+    formfield = ColorField()
+    prepared_value = formfield.prepare_value(value)
+    assert prepared_value == '#' + value
 
 def test_formfield_rendering():
     class TestForm(Form):
